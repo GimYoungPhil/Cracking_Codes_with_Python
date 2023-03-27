283aaa0b23d03f56ee331fee426b28819999160d
YoungPhilGim <youngphil.gim@gmail.com>
2023-03-27 16:40:31 +0900

    fixed

---
+++ src/ch04/reverseCipher_1.py
@@ -0,0 +1,13 @@
+# The Reverse Cipher
+#
+
+message = input('Enter message: ')
+translated = ''
+
+i = len(message) - 1
+while i >= 0:
+    translated = translated + message[i]
+    # print('i is', i, ', message[i] is', message[i], ', tanslaged is', translated)
+    i = i - 1
+
+print(translated)
