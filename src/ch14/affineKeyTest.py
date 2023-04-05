091ad562382c2382291a6cd47d1a6ffb18e213e1
YoungPhilGim <youngphil.gim@gmail.com>
2023-04-05 16:42:36 +0900

    fixed

---
+++ src/ch14/affineKeyTest.py
@@ -0,0 +1,11 @@
+# len(SYMBOLS) ^ 2
+
+
+import affineCipher, cryptomath
+
+message = 'Make things as simple as possible, but not simpler.'
+for keyA in range(2, 80):
+    key = keyA * len(affineCipher.SYMBOLS) + 1
+
+    if cryptomath.gcd(keyA, len(affineCipher.SYMBOLS)) == 1:
+        print(keyA, affineCipher.encryptMessage(key, message))
